8d82fa7126aea7bc383a78bfff76c57a283a6f0f
Jahid Hasan Sagor <56949779+JahidHasanSagor@users.noreply.github.com>
2021-04-04 09:11:01 +0600

    AuthenticateMe initital commit

---
+++ AuthenticateMe/app/src/main/java/com/example/authenticateme/SharedPreferenceManager.java
@@ -0,0 +1,18 @@
+package com.example.authenticateme;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferenceManager {
+
+    SharedPreferences userSession;
+    SharedPreferences.Editor editor;
+    Context context;
+
+    private static final String IS_lOGIN = "IsLoggedIn";
+
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_PASSWORD = "password";
+
+    public Session
+}
